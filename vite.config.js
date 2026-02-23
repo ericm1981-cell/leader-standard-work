204eeee2a433b27ce4aff321ca88af11608f5d68
ericm1981-cell <ericm1981@gmail.com>
2026-02-23 06:57:26 -0500

    Delete vite.config.js

--- vite.config.js
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// Change 'leader-standard-work' to match your GitHub repo name exactly
-export default defineConfig({
-  plugins: [react()],
-  base: '/leader-standard-work/',
-})
